db881f016186dfc42c3ac5a068cba6821033c6ab
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-10 20:39:51 +0800

    Add note api

---
+++ routes/api/note.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\NoteController;
+
+Route::resource('note', NoteController::class)->except('create')->middleware('auth:sanctum');
