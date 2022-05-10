d1e02de27d7de64195ca917c83059c05ae00dc4a
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-10 20:41:42 +0800

    Add note controller

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+# Requests
+use App\Http\Requests\StoreNoteRequest;
+use App\Http\Requests\UpdateNoteRequest;
+
+# Resources
+use App\Http\Resources\NoteResource;
+
+# Service
+use App\Services\NoteService;
+use App\Http\Resources\StoreNoteResource;
+use App\Http\Resources\UpdateNoteResource;
+use App\Http\Resources\ShowNoteResource;
+use App\Http\Resources\EditNoteResource;
+
+class NoteController extends Controller
+{
+    public function __construct(private readonly NoteService $noteService) {}
+
+    public function index()
+    {
+        return NoteResource::collection($this->noteService->getUserNotes());
+    }
+
+    public function store(StoreNoteRequest $request)
+    {
+        $response = $this->noteService->storeNote($request);
+
+        return response(new StoreNoteResource($response));
+    }
+
+    public function show($id)
+    {
+        return response(new ShowNoteResource($this->noteService->showNote($id)));
+    }
+
+    public function edit($id)
+    {
+        return response(new EditNoteResource($this->noteService->showNote($id)));
+    }
+
+    public function update(UpdateNoteRequest $request, $id)
+    {
+        $response = $this->noteService->updateNote($request, $id);
+
+        return response(new UpdateNoteResource($response));
+    }
+
+    public function destroy($id)
+    {
+        return response()->json([
+            'deleted' => $this->noteService->deleteNote($id),
+        ]);
+    }
+}
