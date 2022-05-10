228b5dbe3625fabef4b427c8db172cdd649c201e
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-10 20:40:51 +0800

    Add note model

---
+++ app/Models/Note.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Scopes\NoteScopes;
+
+class Note extends Model
+{
+    use HasFactory, NoteScopes;
+
+    protected $fillable = ['title', 'body'];
+}
