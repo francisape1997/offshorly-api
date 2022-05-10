f8d5e9a57c92c0c244794867849b67674d5db6a0
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-10 20:41:23 +0800

    Add note service

---
+++ app/Services/NoteService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+# Model
+use App\Models\Note;
+
+class NoteService
+{
+    public function __construct(private readonly Note $note) {}
+
+    public function getUserNotes()
+    {
+        return $this->note->owner()->paginate();
+    }
+
+    public function storeNote($request)
+    {
+        return $this->note->create([
+            ...$request->validated(),
+            'created_by' => auth()->user()->id,
+        ]);
+    }
+
+    public function updateNote($request, $id)
+    {
+        $note = Note::where('id', $id)->firstOrFail();
+
+        $note->update($request->validated());
+
+        return $note;
+    }
+
+    public function showNote($id)
+    {
+        return $this->note->findOrFail($id);
+    }
+
+    public function deleteNote($id)
+    {
+        return $this->note->findOrFail($id)->delete();
+    }
+}
