a7ac1f6227e9e87b84816cafdd1e497fbf73b5a2
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-10 20:41:59 +0800

    Add register user request validation

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return
+        [
+            'first_name' => 'required|string|max:255',
+            'last_name'  => 'required|string|max:255',
+            'email'      => 'required|email|unique:users,email',
+            'password'   => 'required|string|confirmed',
+        ];
+    }
+}
